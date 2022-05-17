89700219ca9189f05a19631c36601d3a65ffeee2
nicoche <nicolas@koyeb.com>
2022-05-17 19:57:44 +0200

    Stop ignoring .pdf and .toc
    For the website

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import http.server
 import os
 import socketserver
 
+DIRECTORY = "_website"
+
 # from https://stackoverflow.com/a/52531444
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
 
 if __name__ == '__main__':
     host = os.environ.get("HOST", "0.0.0.0")
     port = os.environ.get("PORT", 8000)
-    directory = os.environ.get("DIRECTORY", "_website")
-
-    class Handler(http.server.SimpleHTTPRequestHandler):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, directory=directory, **kwargs)
 
     with socketserver.TCPServer((host, port), Handler) as httpd:
         print("serving at port", port)
